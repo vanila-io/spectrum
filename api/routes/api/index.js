67cec2d01917d9fbec79916ff821f06889fcd96e
entrptaher@gmail.com <entrptaher@gmail.com>
2019-02-11 00:54:53 +0600

    Enable GraphQL

--- api/routes/api/index.js
+++ api/routes/api/index.js
@@ -3,11 +3,11 @@ import { Router } from 'express';
 
 const apiRouter = Router();
 
-// import graphiql from './graphiql';
-// // Only allow GraphiQL in development
-// if (process.env.NODE_ENV === 'development') {
-//   apiRouter.use('/graphiql', graphiql);
-// }
+import graphiql from './graphiql';
+// Only allow GraphiQL in development
+if (process.env.NODE_ENV === 'development') {
+  apiRouter.use('/graphiql', graphiql);
+}
 
 import slackImporter from './slackImporter';
 apiRouter.use('/slack', slackImporter);
