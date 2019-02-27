ae511673f68c0a7e13d39ee2ada0ec300e250903
entrptaher@gmail.com <entrptaher@gmail.com>
2019-02-27 11:24:46 +0600

    Hardcode fix: Vapid key for web notification

--- src/helpers/web-push-manager.js
+++ src/helpers/web-push-manager.js
@@ -1,5 +1,5 @@
 function urlB64ToUint8Array(base64String) {
-  const padding = '='.repeat((4 - base64String.length % 4) % 4);
+  const padding = '='.repeat((4 - (base64String.length % 4)) % 4);
   const base64 = (base64String + padding).replace(/-/g, '+').replace(/_/g, '/');
 
   const rawData = window.atob(base64);
@@ -37,7 +37,7 @@ class WebPushManager {
     return this.manager.subscribe({
       userVisibleOnly: true,
       applicationServerKey: urlB64ToUint8Array(
-        'BPJAFt0MO2BkTYSuzNdGHbVD6lbk2bzYqMBp1gBXLKUupEIIV7yXViZ1D7SyrJfFbYkKuoxwyaP8YcHU8nRDQsA'
+        'BGd6koDFMvFetgvWYGSyb84AyyhqLDkFIlfrcAyytHsRJsmrT_LEDB7c_5SK0Mt7ztJuYs-y5C7Ey0EC9Y3I90k'
       ),
     });
   };
