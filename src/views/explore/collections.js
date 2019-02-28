b38bc2b64b2d39671297d99305f10e2c25c18281
Stefan Smiljkovic <stefan@vanila.io>
2019-02-28 06:36:02 +0100

    Display communities on explore page
    Hopefully i didnt break anything by removing rest of the code @entrptaher

--- src/views/explore/collections.js
+++ src/views/explore/collections.js
@@ -3,115 +3,19 @@ export const collections = [
     title: 'Top Communities',
     curatedContentType: 'top-communities-by-members',
     communities: [
-      'spectrum',
-      'zeit',
-      'codesandbox',
-      'codepen',
-      'next-js',
-      'figma',
-      'react',
-      'specfm',
-      'product-design',
-      'sketch',
-      'styled-components',
-      'framer',
-      'frontend',
-      'wip',
-      'abstract',
-      'crypto',
-      'sketchcasts',
-      'tech-tea',
-      'design-code',
-      'journalism',
+      'vanila',
+      'uiux',
+      'frontend-hub',
+      'growth-hackers',
+      'javascript',
+      'startup-space',
+      'wekan',
+      'uxstore',
+      'wireflow',
+      'moonly',
     ],
   },
-  {
-    title: 'Design',
-    curatedContentType: 'design-communities',
-    categories: [
-      {
-        title: 'Shop Talk',
-        communities: [
-          'product-design',
-          'icon-design',
-          'typography',
-          'illustrators',
-          'design-management',
-        ],
-      },
-      {
-        title: 'Resources',
-        communities: [
-          'specfm',
-          'up-coming',
-          'sketchcasts',
-          'google-design',
-          'design-code',
-          'codepen',
-          'vectors',
-          'designhunt',
-        ],
-      },
-      {
-        title: 'Tools',
-        communities: [
-          'figma',
-          'sketch',
-          'framer',
-          'abstract',
-          'invision',
-          'compositor',
-          'zeplin',
-          'origami-studio',
-          'fuse',
-        ],
-      },
-    ],
-  },
-  {
-    title: 'Web development',
-    curatedContentType: 'development-communities',
-    categories: [
-      {
-        title: 'Web Frameworks',
-        communities: [
-          'react',
-          'next-js',
-          'node',
-          'codesandbox',
-          'vue-js',
-          'angular',
-          'ember-js',
-          'laravel',
-          'elixir',
-          'styled-components',
-          'graphql',
-          'css-in-js',
-          'electron',
-        ],
-      },
-      {
-        title: 'Native',
-        communities: ['android', 'swiftdev', 'react-native'],
-      },
-      {
-        title: 'Resources',
-        communities: ['frontend', 'specfm'],
-      },
-      {
-        title: 'Tools',
-        communities: [
-          'zeit',
-          'realm',
-          'expo',
-          'compositor',
-          'codepen',
-          'bootstrap',
-          'tachyons',
-        ],
-      },
-    ],
-  },
+  
   // {
   //   title: 'For Product Managers',
   //   categories: [
@@ -125,45 +29,5 @@ export const collections = [
   //     },
   //   ],
   // },
-  {
-    title: 'Tech',
-    curatedContentType: 'tech-communities',
-    categories: [
-      {
-        title: 'Get the latest news',
-        communities: ['tech-tea'],
-      },
-      {
-        title: 'Get on the blockchain',
-        communities: ['balancemymoney', 'crypto', 'btc', 'ethereum'],
-      },
-      {
-        title: 'Explore the future of interfaces',
-        communities: ['augmented-reality', 'voice-interfaces'],
-      },
-    ],
-  },
-  {
-    title: 'Life',
-    curatedContentType: 'life-communities',
-    categories: [
-      {
-        title: 'Do good, feel good',
-        communities: [
-          'for-good',
-          'mental-health',
-          'dev-fit',
-          // '5calls'
-        ],
-      },
-      {
-        title: 'Share your hobbies',
-        communities: ['music', 'photography', 'tabletop-rpg', 'gaming'],
-      },
-      {
-        title: 'Find a new gig',
-        communities: ['careers', 'job-opportunities', 'need-some-work'],
-      },
-    ],
-  },
+  
 ];
